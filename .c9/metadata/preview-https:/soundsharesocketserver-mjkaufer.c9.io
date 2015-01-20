3d72e771424c8c084d785eee13a04eb578164247
Matthew Kaufer <matthew@kaufer.org>
2015-01-20 19:55:44 +0000

    Procfile

---
+++ .c9/metadata/preview-https:/soundsharesocketserver-mjkaufer.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://soundsharesocketserver-mjkaufer.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://soundsharesocketserver-mjkaufer.c9.io","previewer":"preview.browser"},"timestamp":1421350745195,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
---
+++ main.js
@@ -0,0 +1,18 @@
+var ws = require("nodejs-websocket")
+console.log("START");
+// Scream server example: "hi" -> "HI!!!"
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    
+    conn.on("text", function (str) {
+        console.log("Received ",str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
+
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+        console.log(code);
+        console.log(reason);
+    })
+
+}).listen(8080)
